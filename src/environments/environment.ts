7124543928097bbdb541afaa1d52adda9e4c0305
netereal <novik.maria@gmail.com>
2019-06-02 22:37:45 +0300

    initialize firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDy8OckYzlShkbRYWB-tC7P2xZDmSgXEds",
+    authDomain: "todolistapp-5d548.firebaseapp.com",
+    databaseURL: "https://todolistapp-5d548.firebaseio.com",
+    projectId: "todolistapp-5d548",
+    storageBucket: "todolistapp-5d548.appspot.com",
+    messagingSenderId: "999379650628",
+    appId: "1:999379650628:web:ed2143062a64212b"
+  }
 };
 
 /*
